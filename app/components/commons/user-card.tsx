eb6d77f3302dbf5bbc774b4d7de90487c1b02ec7
Jreng7 <jr.waydev@gmail.com>
2025-01-15 22:23:19 -0300

    div principal terminada

--- app/components/commons/user-card.tsx
+++ app/components/commons/user-card.tsx
@@ -1,8 +1,8 @@
 
 export default function UserCard() {
   return (
-    <div className="w-[348px]">
-      <h1>UserCard</h1>
+    <div className="w-[348px] flex flex-col gap-5 items-center p-5 border border-white border-opacity-10 bg-[#121212] rounded-3xl text-white">
+      <div></div>
     </div>
   )
 }
