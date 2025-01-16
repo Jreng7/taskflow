30bc90ad0f18b21af1cda164aba9d0b1d9a1d05b
Jreng7 <jr.waydev@gmail.com>
2025-01-15 22:29:09 -0300

    images

--- app/components/commons/user-card.tsx
+++ app/components/commons/user-card.tsx
@@ -3,7 +3,7 @@ export default function UserCard() {
   return (
     <div className="w-[348px] flex flex-col gap-5 items-center p-5 border border-white border-opacity-10 bg-[#121212] rounded-3xl text-white">
       <div className="size-48">
-        <img src="" alt="" />
+        <img src="/user.png" alt="user" className="rounded-full object-cover w-full h-full" />
       </div>
     </div>
   )
