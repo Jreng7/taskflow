9093dd2603b02214faa67eba44fffd20f799a119
Jr.eng7 <jr.waydev@gmail.com>
2025-01-21 07:34:06 -0300

    card totalvisits finalizado

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "./components/landing-page/hero";
 
 export default function Home() {
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="max-w-7xl m-auto">
       <Hero />
     </div>
   );
