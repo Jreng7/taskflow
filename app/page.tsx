f527405b743dcb49af99643e50a1e0cb011a312a
Jr.eng7 <jr.waydev@gmail.com>
2025-01-29 22:15:27 -0300

    component Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Header from "./components/landing-page/header";
 import Hero from "./components/landing-page/hero";
 
+
 export default function Home() {
-  return (
+  return (Hero
     <div className="max-w-7xl m-auto">
       <Hero />
+      <Header />
     </div>
   );
 }
