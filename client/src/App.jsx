7af27c9278a3bc0d2fd00a384d8669b6d386ba90
Jreng7 <jr.waydev@gmail.com>
2025-03-31 02:56:44 -0300

    style:(App.css) button btn1 and btn2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,20 @@
 import "./App.css";
+import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai'
 
 const arrayTasks = [
   { name: "Limpar a casa"},
   { name: "Comprar cadeira gamer"},
 ]
 
-const Todos = ({ tasks }) => {
+const Todos = ({ dados }) => {
   return (
     <div className="todos">
-      {tasks.map(itemPercorrido => {
+      {dados.map(itemPercorrido => {
         return (
           <div className="todo">
             <p>{itemPercorrido.name}</p>
+            <button className="btn1"><AiOutlineEdit></AiOutlineEdit></button>
+            <button className="btn2"><AiOutlineDelete></AiOutlineDelete></button>
           </div>
         )
       })}
@@ -24,7 +27,7 @@ function App() {
   return (
     <>
       <main className="container">
-        <Todos tasks={arrayTasks}></Todos>
+        <Todos dados={arrayTasks}></Todos>
       </main>
     </>
   );
