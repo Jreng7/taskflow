fb705ce551d257608747a763d57208d5b8dcfb64
Jreng7 <jr.waydev@gmail.com>
2025-03-31 17:59:30 -0300

    feat:(client) função handleNewButton

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,12 @@ import { useEffect, useState } from "react";
 function App() {
 
   const [todos, setTodos] = useState([])
+  const [inputValue, setInputValue] = useState('')
+  const [inputVisibily, setInputVisibility] = useState(false)
+
+  function handleNewButton(){
+    setInputVisibility(!inputVisibily);
+  }
 
   useEffect(() => {
     const fetchTodos = async () => {
@@ -24,11 +30,15 @@ function App() {
 
         <Todos dados={todos}></Todos>
 
-        <input className="inputName" />
-        <button className="newTaskButton">Add Tarefa</button>
+        <input className="inputName" 
+          value={inputValue} 
+          onChange={(event) => {setInputValue(event.target.value)}}
+          style={ {display: inputVisibily ? "block" : "none"} } 
+        />
+        <button onClick={handleNewButton} className="newTaskButton">Add Tarefa</button>
       </main>
     </>
   );
-}
+} 
 
 export default App;
